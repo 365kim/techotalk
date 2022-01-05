3d6f491165527608bfaa511904ca5408121904d5
365kim <365listener@gmail.com>
2022-01-05 19:43:26 +0900

    feat: 테코톡 검색결과 데이터 저장

---
+++ data/index.js
@@ -0,0 +1,15 @@
+import data1 from '../data/data1-1.json';
+import data2 from '../data/data1-2.json';
+import data3 from '../data/data1-3.json';
+import data4 from '../data/data1-4.json';
+import data5 from '../data/data1-5.json';
+import data6 from '../data/data1-6.json';
+
+export const totalData = [
+  ...data1.items,
+  ...data2.items,
+  ...data3.items,
+  ...data4.items,
+  ...data5.items,
+  ...data6.items,
+];
