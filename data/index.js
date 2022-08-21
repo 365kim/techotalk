d4c24d51ece12d1c3f83ad89a8593ffca6cbbb50
365kim <365kim@woowahan.com>
2022-08-21 09:57:26 +0900

    feat: 테코톡 검색결과 데이터 저장

--- data/index.js
+++ data/index.js
@@ -1,15 +1,19 @@
-import data1 from '../data/data1-1.json' assert { type: 'json' };
-import data2 from '../data/data1-2.json' assert { type: 'json' };
-import data3 from '../data/data1-3.json' assert { type: 'json' };
-import data4 from '../data/data1-4.json' assert { type: 'json' };
-import data5 from '../data/data1-5.json' assert { type: 'json' };
-import data6 from '../data/data1-6.json' assert { type: 'json' };
+import data11 from '../data/data1-1.json';
+import data12 from '../data/data1-2.json';
+import data13 from '../data/data1-3.json';
+import data14 from '../data/data1-4.json';
+import data15 from '../data/data1-5.json';
+import data16 from '../data/data1-6.json';
+import data21 from '../data/data2-1.json';
+import data22 from '../data/data2-2.json';
 
 export const totalData = [
-  ...data1.items,
-  ...data2.items,
-  ...data3.items,
-  ...data4.items,
-  ...data5.items,
-  ...data6.items,
+  ...data21.items,
+  ...data22.items,
+  ...data11.items,
+  ...data12.items,
+  ...data13.items,
+  ...data14.items,
+  ...data15.items,
+  ...data16.items,
 ];
